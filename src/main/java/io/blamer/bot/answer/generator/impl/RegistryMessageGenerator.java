a0448e576432cc4acf22ff54661141028970f136
Ivan <clicker.heroes.acg@gmail.com>
2023-04-27 22:51:02 +0300

    Remove useless puzzle

--- src/main/java/io/blamer/bot/answer/generator/impl/RegistryMessageGenerator.java
+++ src/main/java/io/blamer/bot/answer/generator/impl/RegistryMessageGenerator.java
@@ -32,9 +32,7 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
-/*
-* @todo #19 Refactor this class, to send messages into hub
-* */
+
 /**
  * Start conversation.
  */
